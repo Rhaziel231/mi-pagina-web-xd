43c4423e6500dd853cf3a69447997f58fe7a11d9
Rhaziel231 <carlosrrm231@hotmail.com>
2024-06-10 17:08:55 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+function redireccionarPagina() {
+    window.location.href = "https://youtu.be/c7BVtGnlxT8?si=hK2HCqndQppcJtEe";
+  }
+
+
+  function redireccionarPagina2() {
+    window.location.href = " https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+  
+  function redireccionarPagina3() {
+    window.location.href = "file:///C:/Users/Nitro/Desktop/no%20se%20que%20se%20tiene%20que%20hacer/Pelcuhe%20en%20la%20zona%20arqueologica/index.html"
+}
+
+
+function redireccionarPagina4() {
+    window.location.href = "https://supercell.com/en/news/squad-busters-goes-global/"
+}
